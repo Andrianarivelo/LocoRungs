dcbac2d869b6c25bb29e9887c9056a7cd7ee8d1f
Michael Graupner <michael.graupner@parisdescartes.fr>
2017-10-30 21:19:44 +0100

    added missing files

---
+++ tools/h5pyTools.py
@@ -0,0 +1,27 @@
+import ezodf
+import numpy as np
+
+class h5pyTools:
+    def __init__(self):
+            pass
+
+    ############################################################
+    # creates a new data-set if it doesn't exist, otherwise overwrites
+    ############################################################
+    def createOverwriteDS(self,grp,dsname,data,at=None):
+        try:
+            rec = grp.require_dataset(dsname, shape = np.shape(data), dtype = data.dtype, exact=False ) 
+        # either different shape or not exisiting
+        except TypeError:
+            try:
+                del grp[dsname]
+            except KeyError:
+                grp.create_dataset(dsname,data=data,dtype = data.dtype)
+            else:
+                grp.create_dataset(dsname,data=data,dtype = data.dtype)
+        else:
+            rec[:] = data
+        # save attributes
+        if at:
+            grp[dsname].attrs[at[0]]=at[1]
+
