04577775f96fefac174e783d43fc9bf40b489ded
Michael Graupner <michael.graupner@parisdescartes.fr>
2017-10-30 21:27:46 +0100

    file location correction

--- analyzeExperiment.py
+++ analyzeExperiment.py
@@ -199,18 +199,18 @@ if __name__=="__main__":
     # determine location of data files and store location
     if platform.node() == 'thinkpadX1' :
         laptop = True
-        dataBase     = '/media/invivodata/'
         analysisBase = '/media/HDnyc_data/'
     elif platform.node() == 'otillo':
         laptop = False
-        dataBase = '/media/mgraupe/nyc_data/'
+        analysisBase = '/media/mgraupe/nyc_data/'
     elif platform.node() == 'bs-analysis':
         laptop = False
         dataBase = '/home/mgraupe/nyc_data/'
     else:
         print 'Run this script on a server or laptop. Otherwise, adapt directory locations.'
         sys.exit(1)
-    
+        
+    dataBase     = '/media/invivodata/'
     dataLocation  = dataBase + 'altair_data/dataMichael/'
     analysisLocation = analysisBase+'data_analysis/in_vivo_cerebellum_walking/LocoRungsData/'
 
